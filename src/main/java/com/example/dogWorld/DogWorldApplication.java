89f00eb02541599b99833a8aedc79612dcb85c60
strSong0 <str17gogo@naver.com>
2023-11-08 18:25:05 +0900

    feat: first commit - 첫시작

---
+++ src/main/java/com/example/dogWorld/DogWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dogWorld/DogWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
